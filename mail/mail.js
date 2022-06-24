57b5395046c69e8bc7c393b12ca5c58d053fc423
MrGians <gianlumura97@gmail.com>
2022-06-24 15:15:15 +0200

    JS mail v.1

---
+++ mail/mail.js
@@ -0,0 +1,32 @@
+// Mail
+// Creare una lista di indirizzi mail autorizzati.
+// Chiedere email utente
+// Verificare che sia nella lista di indirizzi mail autorizzati
+// Stampare in console un messaggio appropriato sull’esito del controllo. 
+// ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è)
+
+// [Bonus]: Stampare i risultati nel DOM
+// [Bonus]: Provare a prendere la mail da un input e verificare al click su un bottone
+
+console.log("js ok")
+
+const authorizedEmails = ["gianluca@bool.it", "gennaro@bool.it", "pippo@bool.it"];
+const userEmail = prompt("Inserisci la tua Email per accedere", "gianluca@bool.it");
+let accessAllowed;
+
+
+console.table(authorizedEmails);
+console.log("Email utente: " + userEmail);
+
+for (let i = 0; i < authorizedEmails.length; i++) {
+  console.log(authorizedEmails[i]);
+  accessAllowed = "Spiacenti";
+
+
+  if (authorizedEmails[i] == userEmail) {
+    accessAllowed = "Benvenuto";
+  }
+
+}
+
+console.log(accessAllowed);
