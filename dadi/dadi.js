29ffbe17c8bb089796c19f108009cc71efc04e08
MrGians <gianlumura97@gmail.com>
2022-06-24 16:55:38 +0200

    Added Dadi Game [Bonus]

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -1,4 +1,5 @@
 // Dadi
+// Definire variabili Globali
 // Generare un numero random da 1 a 6 per il giocatore 
 // Generare un numero random da 1 a 6 per il computer
 // Stabilire il vincitore in base a chi fa il punteggio più alto
@@ -8,21 +9,37 @@
 // [Bonus]: Stampare i risultati nel DOM
 // [Bonus]: Prendere i numeri random da un input e verificare al click su un bottone
 
-// Genera un numero random da 1 a 6 per il giocatore e per il computer 
-const randomUser = Math.floor(Math.random() * 6) + 1;
-const randomCPU = Math.floor(Math.random() * 6) + 1;
+// Definisco le variabili globali
+const userResult = document.getElementById("user-result");
+const cpuResult = document.getElementById("cpu-result");
+const startGameBtn = document.getElementById("start-game");
+const gameResult = document.getElementById("game-result");
 
-// Stampa in console il numero random di ciascun giocatore
-console.log(`User Number:  ${randomUser}`);
-console.log(`CPU Number: ${randomCPU}`);
+let messageResult = "";
+
+// Al click del Bottone "Start!" verrà eseguito tutto il programma
+startGameBtn.addEventListener("click", function(){
+
+  // Genera un numero random da 1 a 6 per il giocatore e per il computer 
+  const randomUser = Math.floor(Math.random() * 6) + 1;
+  const randomCPU = Math.floor(Math.random() * 6) + 1;
+
+  // SE il tuo numero è maggiore del numero CPU
+  if (randomUser > randomCPU) messageResult = `Complimenti, ${randomUser} è maggiore di ${randomCPU}. Hai Vinto!`;
+
+  // SE il tuo numero è minore del numero CPU
+  if (randomUser < randomCPU) messageResult = `Spiacente, ${randomUser} è minore di ${randomCPU}. Hai Perso!`;
+
+  // SE il tuo numero ed il numero CPU sono uguali
+  if (randomUser == randomCPU) messageResult = `Ops, ${randomUser} e ${randomCPU} sono uguali. Pareggio!`;
 
 
-// SE il tuo numero è maggiore del numero CPU
-if (randomUser > randomCPU) console.log(`Complimenti, ${randomUser} è maggiore di ${randomCPU}. Hai Vinto!`);
+  // Stampa tutto su pagina
+  userResult.innerText = randomUser;
+  cpuResult.innerText = randomCPU;
+  gameResult.innerText = messageResult;
 
-// SE il tuo numero è minore del numero CPU
-if (randomUser < randomCPU) console.log(`Spiacente, ${randomUser} è minore di ${randomCPU}. Hai Perso!`);
+})
 
-// SE il tuo numero ed il numero CPU sono uguali
-if (randomUser == randomCPU) console.log(`Ops, ${randomUser} e ${randomCPU} sono uguali. Pareggio!`);
+
 
